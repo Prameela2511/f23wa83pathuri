6994733dfb03f506ebab15cc8cc283f7961694b6
Prameela2511 <143015943+Prameela2511@users.noreply.github.com>
2023-10-25 20:03:31 -0500

    added mydata

--- app.js
+++ app.js
@@ -8,7 +8,6 @@ var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
 var mydataRouter = require('./routes/mydata');
 
-
 var app = express();
 
 // view engine setup
@@ -24,7 +23,6 @@ app.use(express.static(path.join(__dirname, 'public')));
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
 app.use('/mydata', mydataRouter);
-
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
